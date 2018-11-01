22d766387ea16b62f1283835519ab4908fee54d3
jalalah <jalalah@umd.edu>
2018-10-31 23:04:10 -0400

    Python stub

--- week/8/stub.py
+++ week/8/stub.py
@@ -37,28 +37,37 @@ print("------- HEADER -------")
 print("MAGIC: %s" % hex(magic))
 print("VERSION: %d" % int(version))
 
-offset = 8 #Starting at 8 because we already got to the magic and version
+offset = 8 #Starting at 8 because magic and version were already parsed
 
 timestamp, deadValue = struct.unpack("<LL", data[offset:offset+8])
 
-print("TIMESTAMP: %s" %int(timestamp))
+print("TIMESTAMP: %d" % int(timestamp))
 
-offset= offset + 8
+offset = 12 # 12 now because the timestamp was 4 bytes
 
 author, deadValue = struct.unpack("<LL", data[offset:offset+8])
+print("AUTHOR: %s" % str (author))
 
-print("AUTHOR: %s" %int(author))
-
-offset=offset+8
+offset = 20 # 20 because author was 8 bytes
 
 sectionCount, deadValue = struct.unpack("<LL", data[offset:offset+8])
-
-print("SECTION COUNT: %s" %int(sectionCount))
-
-
-
-# We've parsed the magic and version out for you, but you're responsible for
-# the rest of the header and the actual FPFF body. Good luck!
+print("SECTION COUNT: %d" % int(sectionCount))
 
 print("-------  BODY  -------")
+
+offset = 24 # 24 because that is where the header ends
+
+x = 1
+
+# Trying to go through all the sections here
+
+while (x <= 2):
+	sType,sLen = struct.unpack("<LL", data[offset:offset+8])
+	print ("Section number: %d" %int(x))
+	print("SECTION TYPE: %d"%int(sType))
+	print("SECTION LEN: %d" %int(sLen))
+	x = x+1
+	offset = sLen + 8 # From doc, total size of section = slen + sizeof(sType) + sizeof(sLen). not sure if stype and slen's sizes are 4 bytes each tho. 
+	
+	# Will have to have switch statements here that check what kind of section it is
 

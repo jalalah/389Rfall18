285fa4f5ce10d45578070b33b61623b24088619d
jalalah <jalalah@umd.edu>
2018-11-02 09:44:52 -0400

    Updated code

--- week/8/stub.py
+++ week/8/stub.py
@@ -51,19 +51,17 @@ print("AUTHOR: %s" % str (author))
 offset = 20 # 20 because author was 8 bytes
 
 sectionCount, deadValue = struct.unpack("<LL", data[offset:offset+8])
+
 print("SECTION COUNT: %d" % int(sectionCount))
 
 print("-------  BODY  -------")
 
 offset = 24 # 24 because that is where the header ends
 
-x = 1
-
 # Going through all the sections
 
-while (x <=11):
+while (offset < len(data)):
 	
- 	# When offset = 247093, an error occurs
 	sType,sLen = struct.unpack("<LL", data[offset:offset+8])
 	print("\nSECTION TYPE: %d"%int(sType))
 	print("SECTION LEN: %d" %int(sLen))
@@ -72,31 +70,36 @@ while (x <=11):
 
 	# png NOT DONE
 	if (sType == 1):
-		# 89 50 4E 47 0D 0A 1A 0A
+		# 89 50 4E 47 0D 0A 1A 0A <-- Magic pbytes for png
 		png, = struct.unpack("<%ds" %sLen, data[offset:offset+sLen])
-		pngMagic = "89 50 4E 47 0D 0A 1A 0A" + str(png)
+		pngMagic = "89 50 4E 47 0D 0A 1A 0A "
+		#print(hex(png))
+		file = open("hmrk8", "w")
+		file.write(pngMagic + str(png))
+		file.close()
 		
-	# Array of dwords
+	# Array of dwords. dwords are 8 bytes
 	if (sType == 2):
-
-		dWords = sLen/8
-		dwordOffset = offset
+		dWords = sLen/8  #Amount of dwords there are
+		dOffset = offset
 		print("Amount of dwords %s" %int(dWords))
 
 		while (dWords > 0): 
-			dword1, deadValue = struct.unpack("<LL", data[dwordOffset:dwordOffset+8])
+			# This one makes me have a second argument for my tuple
+			# Even tho a dword is supposed to take all 8 bytes
+			dword1, deadVal = struct.unpack("<LL", data[dOffset:dOffset+8])
 			print(str(dword1))
 			dWords = dWords - 1
-			dwordOffset = dwordOffset + 8
+			dOffset = dOffset + 8
 
 	# UTF-8-enconded text
 	if (sType == 3):
 		UTF, = struct.unpack("<%ds" %sLen, data[offset:offset+sLen])
 		print(str(UTF))
 
-	# Array of doubles
+	# Array of doubles. doubles are 8 bytes
 	if (sType == 4):
-		doubles = (sLen / 8)
+		doubles = (sLen / 8) # Amount of doubles
 		dOffset = offset
 		print("Amount of doubles %d" %int(doubles))
 
@@ -104,40 +107,37 @@ while (x <=11):
 			double1, = struct.unpack("<LL", data[dOffset:dOffset+8])
 			print(str(double1))
 			doubles = doubles - 1
-			dOffset = dOffset + 8	
-	# Array of words
+			dOffset = dOffset + 8
+	
+	# Array of words. words are 4 bytes
 	if (sType == 5):
 		words = (sLen / 4)
-		wordOffset = offset
+		wOffset = offset
 		print("Amount of words %d" %int(words))
 
 		while (words > 0): 
-			word1, deadValue = struct.unpack("<LL", data[wordOffset:wordOffset+8])
+			word1, deadVal = struct.unpack("<LL", data[wOffset:wOffset+8])
 			print(str(word1))
 			words = words - 1
-			wordOffset = wordOffset + 4
+			wOffset = wOffset + 4
 
-	# (lat, long) tuple of doubles
+	# (lat, long) tuple of doubles. 
 	if (sType == 6):
-		#amountOfDoubles = (sLen / 4)
 		dOffset = offset
-		#print("Amount of doubles %d" %int(doubles))
-
-		#while (amountOfDoubles > 0): 
-		word1,word2 = struct.unpack("<LL", data[wordOffset:wordOffset+8])
-		print(str(word1), str(word2))
-		#	amountOfDoubles = amountOfDoubles - 2
-		#	wordOffset = wordOffset + 8
-
+		double1, deadVal1 = struct.unpack("<LL", data[dOffset:dOffset+8])
+		dOffset = dOffset + 8
+		double2, deadVal2 = struct.unpack("<LL", data[dOffset:dOffset+8])
+		print(str(double1), str(double2))
+		
 	# Index of another section
 	if (sType == 7):
 		word, deadValue = struct.unpack("<LL", data[offset: offset+8])
+		print(str(word))
 
 	# asci value
 	if (sType == 9):
 		asci, = struct.unpack("<%ds" %sLen, data[offset:offset+sLen])
 		print(str(asci))
 
-	x = x+1
 	offset = offset + (sLen + 8) 
 	

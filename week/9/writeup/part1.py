8cc3bfc163fc5c80666d8a7a1079f12bfbf3d1db
jalalah <jalalah@umd.edu>
2018-11-09 16:20:18 -0500

    Updated code

---
+++ week/9/writeup/part1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+# importing a useful library -- feel free to add any others you find necessary
+import hashlib
+import string
+
+# this will work if you place this script in your writeup folder
+wordlist = open("../probable-v2-top1575.txt", 'r')
+hashes = open("../hashes", 'r')
+
+# a string equal to 'abcdefghijklmnopqrstuvwxyz'.
+salts = string.ascii_lowercase
+
+for h in hashes: #going through the hashes 
+	for line in wordlist: # Going through the wordlist
+		for salt in salts: # to prepend to passwords
+			password = salt+line.strip('\n') #Prepending onto 
+			encrypt = hashlib.sha512(password) #encrypting
+			if (encrypt.hexdigest() == h.strip('\n')):
+				print("Found a match") 
+				print(password)
+				print(h)

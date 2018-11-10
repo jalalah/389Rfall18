a64de1a7ca8232e6bed5ce2e5c58002f54cc2ef0
jalalah <jalalah@umd.edu>
2018-11-10 08:03:30 -0500

    New code

--- week/9/part2.py
+++ week/9/part2.py
@@ -1,3 +1,5 @@
+#CMSC389R-{H4sh-5l!ngInG-h@sH3r}
+
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
@@ -5,16 +7,76 @@
 import socket
 import hashlib
 
-host = ""   # IP address or URL
-port =      # port
+host = "142.93.117.193"   # IP address or URL
+port = 7331   # port
+
+def return_password(index,data):
+	substring = data[index:] # getting the string starting from the hash algo
+	subarray = substring.split(' ') #splitting the string
+	return subarray[3] #what needs to be hashed
+	
+
+sha512 = 'sha512'
+sha1 = 'sha1'
+sha384 = 'sha384'
+sha224 = 'sha224'
+sha256 = 'sha256'
+md5 = 'md5'
 
 # use these to connect to the service
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 
-# receive some data
-data = s.recv(1024)
-print(data)
+x=11
+
+while (x > 0):
+	# receive some data
+	data = s.recv(1024)
+	print(data)
+
+	if sha512 in data:
+		print("Found a sha512 match")
+		index = data.find(sha512)
+		encrypt = hashlib.sha512((return_password(index,data)).strip('>\n'))
+		s.send(encrypt.hexdigest())
+		s.send("\n")
+
+	elif sha1 in data:
+		print("Found a sha1 match")
+		index = data.find(sha1)
+		encrypt = hashlib.sha1((return_password(index,data)).strip('>\n'))
+		s.send(encrypt.hexdigest())
+		s.send("\n")
+
+	elif sha384 in data:
+		print("Found a sha1 match")
+		index = data.find(sha384)
+		encrypt = hashlib.sha384((return_password(index,data)).strip('>\n'))
+		s.send(encrypt.hexdigest())
+		s.send("\n")
+
+	elif sha224 in data:
+		print("Found a sha1 match")
+		index = data.find(sha224)
+		encrypt = hashlib.sha224((return_password(index,data)).strip('>\n'))
+		s.send(encrypt.hexdigest())
+		s.send("\n")
+
+	elif sha256 in data:
+		print("Found a sha1 match")
+		index = data.find(sha256)
+		encrypt = hashlib.sha256((return_password(index,data)).strip('>\n'))
+		s.send(encrypt.hexdigest())
+		s.send("\n")
+
+	elif md5 in data:
+		print("Found a md5 match")
+		index = data.find(md5)
+		encrypt = hashlib.md5((return_password(index,data)).strip('>\n'))
+		s.send(encrypt.hexdigest())
+		s.send("\n")
+
+	x = x-1
 
 # close the connection
 s.close()
--- week/9/writeup/part1.py
+++ week/9/writeup/part1.py
@@ -8,16 +8,18 @@ import string
 # this will work if you place this script in your writeup folder
 wordlist = open("../probable-v2-top1575.txt", 'r')
 hashes = open("../hashes", 'r')
+w = wordlist.readlines()
 
 # a string equal to 'abcdefghijklmnopqrstuvwxyz'.
 salts = string.ascii_lowercase
 
 for h in hashes: #going through the hashes 
-	for line in wordlist: # Going through the wordlist
+	print(h)
+	for line in w: # Going through the wordlist
 		for salt in salts: # to prepend to passwords
 			password = salt+line.strip('\n') #Prepending onto 
 			encrypt = hashlib.sha512(password) #encrypting
 			if (encrypt.hexdigest() == h.strip('\n')):
-				print("Found a match") 
+				print("Found a match for") 
 				print(password)
 				print(h)
